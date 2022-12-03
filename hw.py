652d821a0e067916e5e04bd179f37b7555aa7675
Azamat Myrzabekov <myrzabekoffazamat@gmail.com>
2022-12-04 00:22:32 +0900

    load textures DONE

--- hw.py
+++ hw.py
@@ -2,6 +2,28 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 import numpy as np
 
+# for loading textures
+from PIL import Image
+
+IMAGE_PATH = 'texture.png'
+
+def load_textures(path = IMAGE_PATH):
+    img = Image.open(path)
+    w, h = img.width, img.height
+
+    # load texture images
+    earth = img.crop((0, h/2, w/4, 3*h/4))
+    sand = img.crop((w/4, h/2, w/2, 3*h/4))
+    stone = img.crop((w/2, h/2, 3*w/4, 3*h/4))
+    earth_wg = img.crop((0, 3*h/4, w/4, h))
+    grass = img.crop((w/4, 3*h/4, w/2, h))
+    brick = img.crop((w/2, 3*h/4, 3*w/4, h))
+
+    # TODO create textures, mb need to create separate classes
+
+
+
+
 class Block:
     def __init__(self):
         pass
@@ -14,25 +36,29 @@ class Viewer:
     def __init__(self):
         pass
 
-    def light(self):
-        glEnable(GL_COLOR_MATERIAL)
-        glEnable(GL_LIGHTING)
-        glEnable(GL_DEPTH_TEST)
+    # NOTE: I think it is not even needed
+    # def light(self):
+    #     glEnable(GL_COLOR_MATERIAL)
+    #     glEnable(GL_LIGHTING)
+    #     glEnable(GL_DEPTH_TEST)
 
-        # feel free to adjust light colors
-        lightAmbient = [0.5, 0.5, 0.5, 1.0]
-        lightDiffuse = [0.5, 0.5, 0.5, 1.0]
-        lightSpecular = [0.5, 0.5, 0.5, 1.0]
-        lightPosition = [1, 1, -1, 0]    # vector: point at infinity
-        glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient)
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse)
-        glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular)
-        glLightfv(GL_LIGHT0, GL_POSITION, lightPosition)
-        glEnable(GL_LIGHT0)
+    #     # feel free to adjust light colors
+    #     lightAmbient = [0.5, 0.5, 0.5, 1.0]
+    #     lightDiffuse = [0.5, 0.5, 0.5, 1.0]
+    #     lightSpecular = [0.5, 0.5, 0.5, 1.0]
+    #     lightPosition = [1, 1, -1, 0]    # vector: point at infinity
+    #     glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient)
+    #     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse)
+    #     glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular)
+    #     glLightfv(GL_LIGHT0, GL_POSITION, lightPosition)
+    #     glEnable(GL_LIGHT0)
 
     def display(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glClearColor(0, 0, 0, 1)
+        # color of clear sky
+        glClearColor(135 / 255, 206 / 255, 235 / 255, 1)
+        # do not render back facing facets
+        glEnable(GL_CULL_FACE)
 
         glMatrixMode(GL_MODELVIEW)
 
@@ -98,11 +124,10 @@ class Viewer:
         glutSpecialFunc(self.special)
         glutMouseFunc(self.mouse)
         glutMotionFunc(self.motion)
-        # for monitoring mouse position while it's not pressed
-        glutPassiveMotionFunc(self.passive_motion)
+        glutPassiveMotionFunc(self.passive_motion) # for monitoring mouse position while it's not pressed
         # glutReshapeFunc(self.reshape)
 
-        self.light()
+        # self.light()
 
         glutMainLoop()
 
